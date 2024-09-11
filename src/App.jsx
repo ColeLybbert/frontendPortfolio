0d89e9cad1049707ef15201332d088e9886318de
Cole <cole.lybbert65@gmail.com>
2024-09-11 00:34:20 -0700

    redo animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,11 @@
-import { AnimatePresence } from 'framer-motion';
-import { useState } from 'react';
 import './App.css'
-import IntroButton from './components.jsx/introButton';
-import Name from "./components.jsx/name";
-import Title from "./components.jsx/title";
+import NavBar from './components.jsx/navBar'
 
 function App() {
 
-  const [show, setShow] = useState(true);
-
-
   return (
     <>
-      <AnimatePresence>
-        {show ? 
-          <>
-        <div className="introCard">
-          <Name />
-          <Title />
-        </div>
-        <div onClick={() => {
-          setShow(!show);
-        }} >
-          <IntroButton />
-        </div >
-        </>
-        : null}
-      </AnimatePresence>
+      <NavBar />
     </>
   )
 }
---
+++ src/components.jsx/navBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <>
+    <div className="navBar">
+    <h1>Cole Lybbert</h1>
+    <h2>Frontend Developer</h2>
+    <button className="contactMe">Contact Me</button>
+    </div>
+    </>
+  )
+}
+
+export default NavBar
